5acf67cc27e65830161f18c783d8b690a158ee41
Alexey Emelyanov <alex3791hunter@gmail.com>
2021-10-01 23:39:22 +0300

    matrix class

--- main.cpp
+++ main.cpp
@@ -1,8 +1,52 @@
 #include <iostream>
-
+#include "matrix.h"
 using namespace std;
 
 int main() {
-
+    int n,m,k,q;
+    cout<<"Input first matrix size:\n";
+    cin>>n>>m;
+    matrix<int> a(n,m);
+    cout<<"Input first matrix:\n";
+    cin>>a;
+    cout<<"Input second matrix size:\n";
+    cin>>n>>m;
+    matrix<int> b(n,m);
+    cout<<"Input second matrix:\n";
+    cin>>b;
+    cout<<"Input an integer for multiplication:\n";
+    cin>>k;
+    cout<<"Input an integer for fast exponentiation:\n";
+    cin>>q;
+    try {
+        cout << "Sum of matrices:\n" << a + b << endl;
+    }
+    catch(const char* msg){
+        cerr<<"Exception caught: "<<msg<<endl;
+    }
+    cout<<"First matrix multiplied by "<<k<<":\n"<<a*k<<endl;
+    try{
+        cout<<"Multiplication of matrices:\n"<<a*b<<endl;
+    }
+    catch(const char* msg){
+        cerr<<"Exception caught: "<<msg<<endl;
+    }
+    cout<<"Transposed first matrix:\n"<<a.transpose()<<endl;
+    try{
+        cout<<"First matrix raised to the "<<q;
+        if(q%10==1){
+            cout<<"st power:\n";
+        }
+        if(q%10==2){
+            cout<<"nd power:\n";
+        }
+        if(q%10==3){
+            cout<<"rd power:\n";
+        }
+        cout<<a.fastpow(q)<<endl;
+    }
+    catch(const char* msg){
+        cerr<<"Exception caught: "<<msg<<endl;
+    }
     return 0;
 }
--- matrix.h
+++ matrix.h
@@ -1,10 +1,144 @@
-//
-// Created by alex3 on 29.09.2021.
-//
-
 #ifndef MATRIX_MATRIX_H
 #define MATRIX_MATRIX_H
 
+#include <iostream>
+#include <vector>
+#include <cassert>
 
+using namespace std;
+
+template <typename T>
+class matrix{
+    int n;
+    int m;
+    vector<vector<T>> data;
+public:
+    matrix(vector<vector<T>>);
+    matrix(int n1,int m1);
+    int size() const { return n; };
+    const matrix operator+(const matrix&) const;
+    const matrix operator*(T) const;
+    const matrix operator*(const matrix&) const;
+    vector<T>& operator[](int i){ return data[i]; };
+    const vector<T>& operator[](int i) const { return data[i]; };
+    template<typename Type> friend istream& operator>>(istream&, matrix<Type>&);
+    template<typename Type> friend ostream& operator<<(ostream&, const matrix<Type>&);
+    const matrix transpose() const;
+    const matrix fastpow(int) const;
+};
+
+template<typename T>
+matrix<T>::matrix(vector<vector<T>> v) {
+    data=v;
+    n=v.size();
+    m=v[0].size();
+}
+
+template<typename T>
+matrix<T>::matrix(int n1,int m1) {
+    n=n1;
+    m=m1;
+    vector<vector<T>> a(n,vector<T>(m,0));
+    data=a;
+}
+
+template<typename T>
+ostream& operator<<(ostream& out, const matrix<T>& a) {
+    for(int i=0;i<a.size();i++) {
+        for (int j = 0; j < a[i].size(); j++) {
+            out << a[i][j] << " ";
+        }
+        out << "\n";
+    }
+    return out;
+}
+
+template<typename T>
+istream &operator>>(istream& in, matrix<T>& a) {
+    for(int i=0;i<a.size();i++) {
+        for (int j = 0; j < a[i].size(); j++) {
+            in >> a[i][j];
+        }
+    }
+    return in;
+}
+
+template<typename T>
+const matrix<T> matrix<T>::operator+(const matrix& a) const {
+    if(a.size()!=n || a[0].size()!=m){
+        throw "Matrices have different sizes";
+    }
+    vector<vector<T>> res(n,vector<T>(m));
+    if(a.size()==n && a[0].size()==m) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                res[i][j] = data[i][j] + a[i][j];
+            }
+        }
+    }
+    return matrix(res);
+}
+
+template<typename T>
+const matrix<T> matrix<T>::operator*(T k) const {
+    vector<vector<T>> res(n,vector<T>(m));
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            res[i][j]=data[i][j]*k;
+        }
+    }
+    return matrix(res);
+}
+
+template<typename T>
+const matrix<T> matrix<T>::operator*(const matrix& a) const {
+    if(m!=a.size()){
+        throw "Matrices have wrong sizes for their multiplication";
+    }
+    vector<vector<T>> res(n,vector<T>(a.m));
+    if(m==a.size()) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < a.m; j++) {
+                for (int r = 0; r < m; r++) {
+                    res[i][j] += data[i][r] * a[r][j];
+                }
+            }
+        }
+    }
+    return matrix(res);
+}
+
+template<typename T>
+const matrix<T> matrix<T>::transpose() const {
+    vector<vector<T>> res(m,vector<T>(n));
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            res[i][j]=data[j][i];
+        }
+    }
+    return matrix(res);
+}
+
+template<typename T>
+const matrix<T> matrix<T>::fastpow(int k) const{
+    matrix<int> res(data);
+    matrix<int> value(data);
+    int q=k-1;
+    if(n!=m){
+        throw "First matrix isn't a square";
+    }
+    if(n==m) {
+        while (q > 0) {
+            if (q % 2 == 1) {
+                res = res * value;
+                q--;
+            } else {
+                q /= 2;
+                value = value * value;
+            }
+        }
+    }
+    return res;
+}
 
 #endif //MATRIX_MATRIX_H
